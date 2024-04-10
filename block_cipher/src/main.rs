7309eedf1cce54366bbc971943ea88fdb5bc6267
bartox7777 <bartosz.zelek@protonmail.com>
2024-04-10 15:46:41 +0200

    Update block_cipher files

--- block_cipher/src/main.rs
+++ block_cipher/src/main.rs
@@ -24,12 +24,28 @@ fn generate_benchmark_file(size_mb: usize) -> String {
     file_name // Return file name
 }
 
-fn encrypt_file(filename: &String, key: &String, iv: &String, cipher: Cipher) -> Vec<u8> {
+fn encrypt_file(filename: &String, key: &String, iv: &String, cipher: Cipher) -> String {
     let mut file = File::open(filename).unwrap(); // Open file
-    let mut data = Vec::new(); // Data
-    file.read_to_end(&mut data).unwrap(); // Read file to data
-    return encrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), &data).unwrap();
-    // Encrypt data and convert to vector
+    let mut data = Vec::new(); // Vector to store data
+    file.read_to_end(&mut data).unwrap(); // Read file to vector
+    let encrypted = encrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), &data).unwrap();
+    let encrypted_filename = format!("enc_{}", filename); // Encrypted file name
+    let mut encrypted_file = File::create(encrypted_filename.as_str()).unwrap();
+    encrypted_file.write_all(&encrypted).unwrap();
+    encrypted_file.sync_all().unwrap();
+    encrypted_filename
+}
+
+fn decrypt_file(filename: &String, key: &String, iv: &String, cipher: Cipher) -> String {
+    let mut file = File::open(filename).unwrap(); // Open file
+    let mut data = Vec::new(); // Vector to store data
+    file.read_to_end(&mut data).unwrap(); // Read file to vector
+    let decrypted = decrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), &data).unwrap();
+    let decrypted_filename = format!("dec_{}", filename); // Decrypted file name
+    let mut decrypted_file = File::create(decrypted_filename.as_str()).unwrap();
+    decrypted_file.write_all(&decrypted).unwrap();
+    decrypted_file.sync_all().unwrap();
+    decrypted_filename
 }
 
 fn main() {
@@ -37,8 +53,63 @@ fn main() {
     let key = String::from("0123456789ABCDEF"); // key must be known only to sender and receiver
     let iv = String::from("FEDCBA9876543210"); // iv don't need to be secret
 
+    println!("Szyfrowanie AES 128 ECB");
+    let start = std::time::Instant::now();
+    let encrypted_filename = encrypt_file(&filename, &key, &iv, Cipher::aes_128_ecb());
+    let end_encrypt = start.elapsed();
+    let start_decrypt = std::time::Instant::now();
+    decrypt_file(&encrypted_filename, &key, &iv, Cipher::aes_128_ecb());
+    let end_decrypt = start_decrypt.elapsed();
+    println!(
+        "Czas szyfrowania: {:?}, czas deszyfrowania: {:?}",
+        end_encrypt, end_decrypt
+    );
+
     println!("Szyfrowanie AES 128 CBC");
     let start = std::time::Instant::now();
-    encrypt_file(&filename, &key, &iv, Cipher::aes_128_cbc());
-    println!("Czas szyfrowania: {:?}", start.elapsed());
+    let encrypted_filename = encrypt_file(&filename, &key, &iv, Cipher::aes_128_cbc());
+    let end_encrypt = start.elapsed();
+    let start_decrypt = std::time::Instant::now();
+    decrypt_file(&encrypted_filename, &key, &iv, Cipher::aes_128_cbc());
+    let end_decrypt = start_decrypt.elapsed();
+    println!(
+        "Czas szyfrowania: {:?}, czas deszyfrowania: {:?}",
+        end_encrypt, end_decrypt
+    );
+
+    println!("Szyfrowanie AES 128 OFB");
+    let start = std::time::Instant::now();
+    let encrypted_filename = encrypt_file(&filename, &key, &iv, Cipher::aes_128_ofb());
+    let end_encrypt = start.elapsed();
+    let start_decrypt = std::time::Instant::now();
+    decrypt_file(&encrypted_filename, &key, &iv, Cipher::aes_128_ofb());
+    let end_decrypt = start_decrypt.elapsed();
+    println!(
+        "Czas szyfrowania: {:?}, czas deszyfrowania: {:?}",
+        end_encrypt, end_decrypt
+    );
+
+    println!("Szyfrowanie AES 128 CFB");
+    let start = std::time::Instant::now();
+    let encrypted_filename = encrypt_file(&filename, &key, &iv, Cipher::aes_128_cfb1());
+    let end_encrypt = start.elapsed();
+    let start_decrypt = std::time::Instant::now();
+    decrypt_file(&encrypted_filename, &key, &iv, Cipher::aes_128_cfb1());
+    let end_decrypt = start_decrypt.elapsed();
+    println!(
+        "Czas szyfrowania: {:?}, czas deszyfrowania: {:?}",
+        end_encrypt, end_decrypt
+    );
+
+    println!("Szyfrowanie AES 128 CTR");
+    let start = std::time::Instant::now();
+    let encrypted_filename = encrypt_file(&filename, &key, &iv, Cipher::aes_128_ctr());
+    let end_encrypt = start.elapsed();
+    let start_decrypt = std::time::Instant::now();
+    decrypt_file(&encrypted_filename, &key, &iv, Cipher::aes_128_ctr());
+    let end_decrypt = start_decrypt.elapsed();
+    println!(
+        "Czas szyfrowania: {:?}, czas deszyfrowania: {:?}",
+        end_encrypt, end_decrypt
+    );
 }
